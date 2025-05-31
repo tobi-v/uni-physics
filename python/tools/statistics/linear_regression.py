a59ad65c162657d80532ac777a7ef4f59a0c4c29
torave <t.vetter@live.de>
2025-05-31 10:44:36 +0200

    Adjust linreg

--- python/tools/statistics/linear_regression.py
+++ python/tools/statistics/linear_regression.py
@@ -3,7 +3,7 @@ from numpy import polyfit, poly1d
 ### Also returns the coefficients of the curve and the covariance of the fit
 def linreg(x, y):
     coeff, cov = polyfit(x, y, 1, cov=True)
-    return poly1d(coeff), coeff[0], cov
+    return poly1d(coeff), coeff, cov
 
 def plotWithErrorBars(ax, x, y, linreg, x_absErr=0, y_absErr=0, title="", xlabel="", ylabel=""):
     ax.set_title(title)
