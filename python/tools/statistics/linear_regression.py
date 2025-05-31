3639fe2bec4b67747cc90b24459b31322fa7c2bd
torave <t.vetter@live.de>
2025-05-31 19:13:50 +0200

    Sm0l change to test ci

--- python/tools/statistics/linear_regression.py
+++ python/tools/statistics/linear_regression.py
@@ -5,9 +5,9 @@ def linreg(x, y):
     coeff, cov = polyfit(x, y, 1, cov=True)
     return poly1d(coeff), coeff, cov
 
-def plotWithErrorBars(ax, x, y, linreg, x_absErr=0, y_absErr=0, title="", xlabel="", ylabel=""):
+def plotWithErrorBars(ax, x, y, linregFunc, x_absErr=0, y_absErr=0, title="", xlabel="", ylabel=""):
     ax.set_title(title)
     ax.set_xlabel(xlabel)
     ax.set_ylabel(ylabel)
-    ax.plot(x, linreg(x), '--k')
+    ax.plot(x, linregFunc(x), '--k')
     ax.errorbar(x, y, fmt='k.', xerr=x_absErr, yerr=y_absErr, ecolor='r', capsize=1.5)
