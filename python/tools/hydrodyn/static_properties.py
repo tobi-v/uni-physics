833be0120539ca651b57ee0fb00a125bfee77fb5
torave <t.vetter@live.de>
2025-05-31 10:44:26 +0200

    Adjust static hydrodyn formulae

--- python/tools/hydrodyn/static_properties.py
+++ python/tools/hydrodyn/static_properties.py
@@ -1,9 +1,11 @@
-from tools.statistics.uncertainty_calculation import GaussianErrorPropagation
+from tools.statistics.uncertainty_calculation import GetResultAndUncertainty
 
 def GravitationalPressure(rho, g, h, uncertainty=False, delRho=0, delG=0, delH=0):
   def GravitationalPressureInner(rho, g, h):
     return rho*g*h
-  if uncertainty:
-    propagated_uncertainty = GaussianErrorPropagation(GravitationalPressureInner, [rho, g, h], [delRho, delG, delH])
-    return GravitationalPressureInner(rho, g, h), propagated_uncertainty
-  return GravitationalPressureInner(rho, g, h)
+  return GetResultAndUncertainty(GravitationalPressureInner, [rho, g, h], uncertainty, [delRho, delG, delH])
+
+def RhoFromGravitationalPresure(p, g, h, uncertainty=False, delP=0, delG=0, delH=0):
+  def RhoFromGravitationalPresureInner(p, g, h):
+    return p/(g*h)
+  return GetResultAndUncertainty(RhoFromGravitationalPresureInner, [p, g, h], uncertainty, [delP, delG, delH])
