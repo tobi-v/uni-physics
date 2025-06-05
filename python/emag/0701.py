9cc61918f1e3e568f1d6b7a018044e070c54bb36
Tobias Vetter <tobias.vetter@dlr.de>
2025-06-05 09:57:49 +0200

    Add solution for part 2

--- python/emag/0701.py
+++ python/emag/0701.py
@@ -2,16 +2,16 @@
 
 from sympy import Eq, simplify, solve, symbols
 
-given = symbols('u alpha')
-u, alpha = given
-searched = symbols('u1 u2 u3 u4 u5 u6')
+given = symbols('U alpha')
+U, alpha = given
+searched = symbols('U1 U2 U3 U4 U5 U6')
 U1, U2, U3, U4, U5, U6 = searched
 eqs = [
     # voltage
-    Eq(U1 + U2 + U5, u),
+    Eq(U1 + U2 + U5, U),
     Eq(U2 + U4, U3),
     Eq(U4 + U6, U5),
-    Eq(U1 + U3 + U6, u),
+    Eq(U1 + U3 + U6, U),
     # current
     Eq(U2 + U3, U1),
     Eq(U4 + U5, U2),
@@ -22,4 +22,9 @@ eqs = [
 # 1. Solve for voltages
 sol = solve(eqs, searched, dict=True)
 simple_sol = {key: simplify(s) for key, s in sol[0].items()}
-print(simple_sol)
+print(f"\nVolagtes: {simple_sol}")
+
+# 2. Find equivalent resistance
+R1, equi_R = symbols('R1, equi_R')
+equi_R = simplify(R1 * U / simple_sol[U1])
+print(f"\nEquivalent resistance: {equi_R}\n")
