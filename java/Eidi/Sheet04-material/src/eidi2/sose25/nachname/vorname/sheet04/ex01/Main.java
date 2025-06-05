5616eeb46e2d3db06d396885e1c4b993266f5238
tobi-v <tobias.vetter@uni-ulm.de>
2025-06-05 11:31:47 +0200

    Copy files for sheet05

--- java/Eidi/Sheet04-material/src/eidi2/sose25/nachname/vorname/sheet04/ex01/Main.java
+++ java/Eidi/Sheet04-material/src/eidi2/sose25/nachname/vorname/sheet04/ex01/Main.java
@@ -3,7 +3,8 @@ package eidi2.sose25.nachname.vorname.sheet04.ex01;
 public class Main {
     
    public static void main(String[] args) {
-
+      double[] array = new double[5];
+      DynamicArray<Integer> da = new DynamicArray(array);
    }
 
 }
---
+++ java/Eidi/Sheet05-material/src/main/java/org/example/Something.java
@@ -0,0 +1,38 @@
+package eidi2_ex05;
+
+public class Something implements Comparable<Something> {
+	
+	protected int number;
+	
+	public Something(int i) {
+		this.number = i;
+	}
+
+	public int getNumber() {
+		return number;
+	}
+
+	public void setNumber(int number) {
+		this.number = number;
+	}
+
+	@Override
+	public String toString() {
+		return "[" + number + "]";
+	}
+
+	@Override
+	public int compareTo(Something o) {
+		return this.number - o.getNumber();
+	}
+	
+	@Override
+	public boolean equals(Object o) {
+		if(o == null || !(o instanceof Something)) {
+			return false;
+		} else {
+			return (this.number - ((Something) o).getNumber()) == 0;
+		}
+	}
+
+}
