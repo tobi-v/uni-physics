a8d2c0186defcd5e5c3bf8d975f921bfad58fbe3
Tobias Vetter <tobias.vetter@dlr.de>
2025-06-05 09:50:19 +0200

    Beautify somewhat

--- python/emag/0701.py
+++ python/emag/0701.py
@@ -1,23 +1,25 @@
 # Stolen form ct2034
 
-from pprint import pprint
+from sympy import Eq, simplify, solve, symbols
 
-from sympy import symbols, Eq, solve, Matrix, linsolve
-
-vars = symbols('u u1 u2 u3 u4 u5 u6 alpha')
-u, u1, u2, u3, u4, u5, u6, alpha = vars
+given = symbols('u alpha')
+u, alpha = given
+searched = symbols('u1 u2 u3 u4 u5 u6')
+U1, U2, U3, U4, U5, U6 = searched
 eqs = [
     # voltage
-    Eq(u1 + u2 + u5, u),
-    Eq(u2 + u4, u3),
-    Eq(u4 + u6, u5),
-    Eq(u1 + u3 + u6, u),
+    Eq(U1 + U2 + U5, u),
+    Eq(U2 + U4, U3),
+    Eq(U4 + U6, U5),
+    Eq(U1 + U3 + U6, u),
     # current
-    Eq(u2 + u3, u1),
-    Eq(u4 + u5, u2),
-    Eq((u3 + u4)/alpha, u6),
-    Eq(u5 + u6 * alpha, u1),
+    Eq(U2 + U3, U1),
+    Eq(U4 + U5, U2),
+    Eq((U3 + U4)/alpha, U6),
+    Eq(U5 + U6 * alpha, U1),
 ]
 
-sol = solve(eqs, vars)
-pprint(sol)
+# 1. Solve for voltages
+sol = solve(eqs, searched, dict=True)
+simple_sol = {key: simplify(s) for key, s in sol[0].items()}
+print(simple_sol)
