9afa290cd1f9ce4bb9fd4ba2067883b29c26b3e2
Tobias Vetter <tobias.vetter@dlr.de>
2025-06-06 08:17:19 +0200

    Implement methods for magnetic field of loops and helmholtz loops

--- python/tools/electricity/magnetic_field.py
+++ python/tools/electricity/magnetic_field.py
@@ -1,13 +1,12 @@
-from numpy import array, cross, pi
+from numpy import cross, ndarray, pi
 from numpy.linalg import norm
-
-mu_0 = 1.257e-6 # [N/A²]
+from scipy.constants import mu_0
 
 def BiotSavart(current,
-               wire_segment: array,
-               delta_r: array,
+               wire_segment: ndarray,
+               delta_r: ndarray,
                exclude_singularities=True,
-               singularity_limit = 1e-3) -> array:
+               singularity_limit = 1e-3) -> ndarray:
     '''
     delta_r: distance from wire vector
     
@@ -16,3 +15,17 @@ def BiotSavart(current,
     if(exclude_singularities and (distance < singularity_limit)):
         distance = distance + 1e-3
     return (mu_0*current/(4*pi)) * cross(wire_segment, delta_r) / (distance)**3
+
+def BOfLoopAlongZ(z: ndarray, current: float, radius: float) -> ndarray:
+    """Magnetic field B_z along the z-axis from a single circular current loop.
+    Returns: B_z field (T) at position(s) z
+    """
+    return mu_0 *current*radius**2 / (2*(z**2 + radius**2)**(3/2))
+
+def HelmholtzAlongZ(z: ndarray, current: float, radius: float, distance: float) -> ndarray:
+    """B_z field along the z-axis from two identical coaxial current loops (Helmholtz pair).
+
+    Returns: B_z field (T) at position(s) z
+    """
+    return BOfLoopAlongZ(z+distance/2, current, radius) \
+            + BOfLoopAlongZ(z-distance/2, current, radius)
