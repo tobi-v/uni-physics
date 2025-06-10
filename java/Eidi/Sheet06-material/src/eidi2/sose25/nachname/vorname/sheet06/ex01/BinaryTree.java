505723934bb70444af7bbc9b988940f1fc872d30
Tobias Vetter <tobias.vetter@dlr.de>
2025-06-10 13:15:57 +0200

    Solve 1b i add node recursively to tree

--- java/Eidi/Sheet06-material/src/eidi2/sose25/nachname/vorname/sheet06/ex01/BinaryTree.java
+++ java/Eidi/Sheet06-material/src/eidi2/sose25/nachname/vorname/sheet06/ex01/BinaryTree.java
@@ -1,5 +1,49 @@
 package eidi2.sose25.nachname.vorname.sheet06.ex01;
 
-public class BinaryTree <T extends Comparable<T>> {
-	Node<T> root;
+public class BinaryTree <T extends Comparable<T>> implements Tree<T> {
+	private Node<T> root;
+
+	@Override
+	public boolean add(T value){
+		if (root == null){ root = new Node<T>(value); return true;}
+		return addNode(root, value);
+	}
+
+	private boolean addNode(Node<T> node, T value){
+		if (value.compareTo(node.value) < 0){
+			if (node.left == null){
+				node.left = new Node<T>(value);
+			}
+			return addNode(node.left, value);
+		}
+		if (value.compareTo(node.value) > 0){
+			if (node.right == null){
+				node.right = new Node<T>(value);
+			}
+			return addNode(node.right, value);
+		}
+		return false;
+	}
+
+	@Override
+	public boolean addIt(T value){
+		return false;
+	}
+
+	@Override
+	public String preorder(){return "";}
+
+	@Override
+	public String inorder(){return "";}
+
+	@Override
+	public String postorder(){return "";}
+
+	@Override
+	public int size(){return 0;}
+
+	@Override
+	public boolean remove(T value){
+		return false;
+	}
 }
