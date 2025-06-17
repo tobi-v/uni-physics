179f07c3d03fdfb6a0a1ba70a20ab1f5764ac250
Tobias Vetter <tobias.vetter@dlr.de>
2025-06-17 12:59:01 +0200

    Solve 1d implement mutate and flat

--- java/Eidi/Sheet06-material/src/eidi2/sose25/nachname/vorname/sheet06/ex01/BinaryTree.java
+++ java/Eidi/Sheet06-material/src/eidi2/sose25/nachname/vorname/sheet06/ex01/BinaryTree.java
@@ -1,25 +1,36 @@
 package eidi2.sose25.nachname.vorname.sheet06.ex01;
 
-public class BinaryTree <T extends Comparable<T>> implements Tree<T> {
+import java.util.function.UnaryOperator;
+import java.util.function.BiFunction;
+
+public class BinaryTree<T extends Comparable<T>> implements Tree<T>, Functional<T> {
 	private Node<T> root;
 	private int size;
 
 	@Override
-	public boolean add(T value){
-		if (root == null){ root = new Node<T>(value); size++; return true;}
+	public boolean add(T value) {
+		if (root == null) {
+			root = new Node<T>(value);
+			size++;
+			return true;
+		}
 		return addNode(root, value);
 	}
 
-	private boolean addNode(Node<T> node, T value){
-		if (value.compareTo(node.value) < 0){
-			if (node.left == null){
-				node.left = new Node<T>(value); size++; return true;
+	private boolean addNode(Node<T> node, T value) {
+		if (value.compareTo(node.value) < 0) {
+			if (node.left == null) {
+				node.left = new Node<T>(value);
+				size++;
+				return true;
 			}
 			return addNode(node.left, value);
 		}
-		if (value.compareTo(node.value) > 0){
-			if (node.right == null){
-				node.right = new Node<T>(value); size++; return true;
+		if (value.compareTo(node.value) > 0) {
+			if (node.right == null) {
+				node.right = new Node<T>(value);
+				size++;
+				return true;
 			}
 			return addNode(node.right, value);
 		}
@@ -27,29 +38,40 @@ public class BinaryTree <T extends Comparable<T>> implements Tree<T> {
 	}
 
 	@Override
-	public boolean addIt(T value){
-		if (root == null){ root = new Node<T>(value); size++; return true;}
+	public boolean addIt(T value) {
+		if (root == null) {
+			root = new Node<T>(value);
+			size++;
+			return true;
+		}
 		Node<T> node = root;
-		for(int ii = 0; ii < size; ii++){
-			if (value.compareTo(node.value) < 0){
-				if (node.left == null){
-					node.left = new Node<T>(value); size++; return true;
+		for (int ii = 0; ii < size; ii++) {
+			if (value.compareTo(node.value) < 0) {
+				if (node.left == null) {
+					node.left = new Node<T>(value);
+					size++;
+					return true;
 				}
-				node = node.left; continue;
+				node = node.left;
+				continue;
 			}
-			if (value.compareTo(node.value) > 0){
-				if (node.right == null){
-					node.right = new Node<T>(value); size++; return true;
+			if (value.compareTo(node.value) > 0) {
+				if (node.right == null) {
+					node.right = new Node<T>(value);
+					size++;
+					return true;
 				}
-				node = node.right; continue;
+				node = node.right;
+				continue;
 			}
 		}
 		return false;
 	}
 
 	@Override
-	public String preorder(){
-		if(root == null) return "";
+	public String preorder() {
+		if (root == null)
+			return "";
 		StringBuilder sb = new StringBuilder();
 		sb.append("[");
 		printSubTreePreorder(sb, root);
@@ -58,16 +80,21 @@ public class BinaryTree <T extends Comparable<T>> implements Tree<T> {
 		return sb.toString();
 	}
 
-	private void printSubTreePreorder(StringBuilder sb, Node<T> node){
+	private void printSubTreePreorder(StringBuilder sb, Node<T> node) {
 		sb.append(node.value.toString());
 		sb.append(", ");
-		if(node.left != null){ printSubTreePreorder(sb, node.left);}
-		if(node.right != null){ printSubTreePreorder(sb, node.right);}
+		if (node.left != null) {
+			printSubTreePreorder(sb, node.left);
+		}
+		if (node.right != null) {
+			printSubTreePreorder(sb, node.right);
+		}
 	}
 
 	@Override
-	public String inorder(){
-		if(root == null) return "";
+	public String inorder() {
+		if (root == null)
+			return "";
 		StringBuilder sb = new StringBuilder();
 		sb.append("[");
 		printSubTreeInorder(sb, root);
@@ -76,16 +103,21 @@ public class BinaryTree <T extends Comparable<T>> implements Tree<T> {
 		return sb.toString();
 	}
 
-	private void printSubTreeInorder(StringBuilder sb, Node<T> node){
-		if(node.left != null){ printSubTreeInorder(sb, node.left);}
+	private void printSubTreeInorder(StringBuilder sb, Node<T> node) {
+		if (node.left != null) {
+			printSubTreeInorder(sb, node.left);
+		}
 		sb.append(node.value.toString());
 		sb.append(", ");
-		if(node.right != null){ printSubTreeInorder(sb, node.right);}
+		if (node.right != null) {
+			printSubTreeInorder(sb, node.right);
+		}
 	}
 
 	@Override
-	public String postorder(){
-		if(root == null) return "";
+	public String postorder() {
+		if (root == null)
+			return "";
 		StringBuilder sb = new StringBuilder();
 		sb.append("[");
 		printSubTreePostorder(sb, root);
@@ -94,32 +126,89 @@ public class BinaryTree <T extends Comparable<T>> implements Tree<T> {
 		return sb.toString();
 	}
 
-	private void printSubTreePostorder(StringBuilder sb, Node<T> node){
-		if (node.left != null) printSubTreePostorder(sb, node.left);
-		if (node.right != null) printSubTreePostorder(sb, node.right);
+	private void printSubTreePostorder(StringBuilder sb, Node<T> node) {
+		if (node.left != null)
+			printSubTreePostorder(sb, node.left);
+		if (node.right != null)
+			printSubTreePostorder(sb, node.right);
 		sb.append(node.value.toString());
 		sb.append(", ");
 	}
 
 	@Override
-	public int size(){return size;}
+	public int size() {
+		return size;
+	}
 
-	private void AddCurrentAndChildrenIfNotVal(BinaryTree<T> tree, Node<T> node, T val){
-		if(!node.value.equals(val)) tree.add(node.value);
-		if(node.left != null)	AddCurrentAndChildrenIfNotVal(tree, node.left, val);
-		if(node.right != null) AddCurrentAndChildrenIfNotVal(tree, node.right, val);
+	private void AddCurrentAndChildrenIfNotVal(BinaryTree<T> tree, Node<T> node, T val) {
+		if (!node.value.equals(val))
+			tree.add(node.value);
+		if (node.left != null)
+			AddCurrentAndChildrenIfNotVal(tree, node.left, val);
+		if (node.right != null)
+			AddCurrentAndChildrenIfNotVal(tree, node.right, val);
 	}
 
 	@Override
-	public boolean remove(T value){
-		if(root == null) return false;
+	public boolean remove(T value) {
+		if (root == null)
+			return false;
 		BinaryTree<T> newTree = new BinaryTree<>();
 		AddCurrentAndChildrenIfNotVal(newTree, root, value);
-		if(size == newTree.size) return false;
-		else{
+		if (size == newTree.size)
+			return false;
+		else {
 			root = newTree.root;
 			size = newTree.size;
 			return true;
 		}
+	}
+
+	private void traverseInOrder(UnaryOperator<T> f, Node<T> node) {
+		if (node.left != null) {
+			traverseInOrder(f, node.left);
+		}
+		node.value = f.apply(node.value);
+		if (node.right != null) {
+			traverseInOrder(f, node.right);
+		}
+	}
+
+	private <R> R traverseInOrder(BiFunction<T, R, R> f, Node<T> node, R initial) {
+		if (node.left != null) {
+			initial = traverseInOrder(f, node.left, initial);
+		}
+		initial = f.apply(node.value, initial);
+		if (node.right != null) {
+			initial = traverseInOrder(f, node.right, initial);
+		}
+		return initial;
+	}
+
+	/**
+	 * Replace each element "t" with the result of f.apply(t).
+	 * 
+	 * @param f The function to apply on each element.
+	 */
+	@Override
+	public void mutate(UnaryOperator<T> f) {
+		traverseInOrder(f, root);
+	}
+
+	/**
+	 * Combine all elements using the function f.
+	 * 
+	 * @param <R>     Type of the return value.
+	 * @param f       The function used to combine all elements.
+	 *                It is applied to an element "t" and the result of a previous
+	 *                application "r" using f.apply(t,r).
+	 * @param initial The initial value used for the first application of f or as
+	 *                the result for an empty data structure.
+	 *                It can also serve as an accumulator for recursive calls.
+	 * @return
+	 */
+	@Override
+	public <R> R flat(BiFunction<T, R, R> f, R initial) {
+		return traverseInOrder(f, root, initial);
 	}
 }
