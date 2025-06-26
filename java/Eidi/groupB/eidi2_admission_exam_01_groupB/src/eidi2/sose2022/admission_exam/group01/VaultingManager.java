ffca15f34d559c94db438870a8039f65e4e848f3
tobi-v <tobias.vetter@uni-ulm.de>
2025-06-26 11:47:34 +0200

    Adds file for second test exam

---
+++ java/Eidi/groupB/eidi2_admission_exam_01_groupB/src/eidi2/sose2022/admission_exam/group01/VaultingManager.java
@@ -0,0 +1,39 @@
+package eidi2.sose2022.admission_exam.group01;
+
+import java.util.List;
+import java.util.LinkedList;
+
+import eidi2.sose2022.admission_exam.group01.util.Tuple;
+import eidi2.sose2022.admission_exam.storage.IStoreable;
+import eidi2.sose2022.admission_exam.storage.Vault;
+
+public class VaultingManager {
+	final List<Tuple<Bank, List<Vault<IStoreable>>>> banksAndVaults = new LinkedList<>();
+	
+	/**
+	 * Searches for a bank and its associated vaults and returns both of them in the form of a tuple.
+	 * @param bankToFind the bank for which a mapping tuple should be found
+	 * @return Tuple containing the sought bank and a list of its associated Vaults or NULL if the bank does not exist in the mappings
+	 */
+	private Tuple<Bank, List<Vault<IStoreable>>> findBankTuple(Bank bankToFind) {
+		return banksAndVaults.stream().filter(t -> t.first == bankToFind).findFirst().orElseGet(null);
+	}
+	
+	public void addVaultForBank(Bank bank, Vault<IStoreable> vaultToAdd) {
+		//TODO
+	}
+	
+	/**
+	 * Searches for a vault with the passed ID.
+	 * @param idToFind
+	 * @return the vault with the passed ID or null if no such vault was found.
+	 */
+	Vault<IStoreable> findVaultWithID(long idToFind) {
+		return banksAndVaults.stream().flatMap(t -> t.second.stream()).filter(v -> v.getID() == idToFind).findFirst().orElse(null);
+	}
+	
+	public boolean addStoreableToBankVault(long vaultID, IStoreable storeableToAdd) {
+		//TODO
+		return false;
+	}
+}
