adef481e39831972f22d87339937ca4318065838
tobi-v <57125403+tobi-v@users.noreply.github.com>
2025-07-01 13:03:26 +0200

    Ex/eidi/sheet06 (#21)

---
+++ java/Eidi/Sheet06-material/src/eidi2/sose25/nachname/vorname/sheet06/ex02/GradeOverview.java
@@ -0,0 +1,34 @@
+package eidi2.sose25.nachname.vorname.sheet06.ex02;
+
+import java.util.HashMap;
+import java.util.Optional;
+
+public class GradeOverview {
+	private final HashMap<String, Pair<Double, Integer>> GRADEOVERVIEW = new HashMap<>();
+	
+	public void addTestResult(String lectureName, Pair<Double, Integer> gradeAndECTS) {
+		GRADEOVERVIEW.put(lectureName, gradeAndECTS);
+	}
+	
+	public int currentECTS() {
+		int accumulatedECTS = 0;
+		for(Pair<Double, Integer> p : GRADEOVERVIEW.values())
+			accumulatedECTS += p.second;
+		return accumulatedECTS;
+	}
+	
+	public Optional<Pair<Double, Integer>> getExamResult(String lectureName) {
+		return Optional.ofNullable(GRADEOVERVIEW.get(lectureName));
+	}
+	
+	public double totalGradeAverage() {
+		int totalECTS = 0;
+		double weightedGradeSum = 0;
+		for(Pair<Double, Integer> p : GRADEOVERVIEW.values()){
+				weightedGradeSum += p.first*p.second;
+				totalECTS += p.second;
+		}
+
+		return weightedGradeSum/totalECTS;
+	}
+}
