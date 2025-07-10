a68c1abb63ab2c714da398c6f70ffbc97c28cdce
tobi-v <tobias.vetter@uni-ulm.de>
2025-07-10 11:49:05 +0200

    Materials for sheet 10

---
+++ java/Eidi/Sheet10-material/src/eidi2/sose25/nachname/vorname/sheet10/ex02/PatternMatching.java
@@ -0,0 +1,44 @@
+package eidi2.sose25.nachname.vorname.sheet10.ex02;
+
+
+public abstract class PatternMatching {
+
+    ///////////////
+    //BRUTE-FORCE//
+    ///////////////
+
+    public static int[] bruteForce(String pattern, String text) {
+        //TODO: a)
+        return null;
+    }
+
+    
+    ///////////////
+    //BOYER-MOORE//
+    ///////////////
+
+    public static int[] computeDelta1(String pattern) {
+        //TODO: b)
+        return null;
+    }
+
+    public static int[] boyerMoore(String pattern, String text) {
+        //TODO: b)
+        return null;
+    }
+
+
+    //////////////////////
+    //KNUTH-MORRIS-PRATT//
+    //////////////////////
+
+    public static int[] computePrefix(String pattern) {
+        //TODO: c)
+        return null;
+    }
+
+    public static int[] kmp(String pattern, String text) {
+        //TODO: c)
+        return null;
+    }
+}
