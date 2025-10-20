0bfe09079834b14f194f4cd03081fc49d249c9ff
Tobias Vetter <tobias.vetter@dlr.de>
2025-10-20 10:27:17 +0200

    Add labels to plotting

--- python/tools/python/plot.py
+++ python/tools/python/plot.py
@@ -1,13 +1,13 @@
-def DefaultPlot(ax, x, y, title="", xlabel="", ylabel=""):
+def DefaultPlot(ax, x, y, title="", label="", xlabel="", ylabel=""):
     ax.set_title(title)
     ax.set_xlabel(xlabel)
     ax.set_ylabel(ylabel)
-    ax.plot(x, y, '--k')
+    ax.plot(x, y, '--k', label=label)
     ax.grid()
 
-def DefaultScatter(ax, x, y, title="", xlabel="", ylabel=""):
+def DefaultScatter(ax, x, y, title="", label="", xlabel="", ylabel=""):
     ax.set_title(title)
     ax.set_xlabel(xlabel)
     ax.set_ylabel(ylabel)
-    ax.scatter(x, y, c='k')
+    ax.scatter(x, y, c='k', label=label)
     ax.grid()
