92336ead517c4aea97bf3c14d7cf91174515aa01
tobi-v <t_vetter@live.de>
2025-11-05 20:38:25 +0100

    Scatter plot with error bars

--- python/tools/python/plot.py
+++ python/tools/python/plot.py
@@ -13,3 +13,12 @@ def DefaultScatter(ax, x, y, title="", label="", xlabel="", ylabel="", c='k'):
     ax.scatter(x, y, c=c, label=label)
     ax.grid(visible=True)
     ax.legend()
+    
+def ScatterWithErrorBars(ax, x, y,
+                      x_absErr=0, y_absErr=0, title="", label="", xlabel="", ylabel="", data_color="k.", error_color="r", legend_loc='best', grid=True):
+    ax.set_title(title)
+    ax.set_xlabel(xlabel)
+    ax.set_ylabel(ylabel)
+    ax.errorbar(x, y, fmt=data_color, xerr=x_absErr, yerr=y_absErr, label=label, ecolor=error_color, capsize=1.5)
+    ax.legend(loc=legend_loc)
+    ax.grid(visible=grid)
