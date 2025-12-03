10ed3dd34e2beab4c7d4e5352cb28f3474345ef3
tobi-v <57125403+tobi-v@users.noreply.github.com>
2025-12-03 20:24:05 +0100

    Ex/prakt/28 schwingkreis korrekturen (#32)
    * Update requirements
    
    * Adjust naming and typing
    
    * Adjust plots
    
    * fontisze adjustments

--- python/tools/maths/functions.py
+++ python/tools/maths/functions.py
@@ -1,12 +1,12 @@
-from numpy import exp
+from numpy import exp, floating
 from numpy.typing import NDArray
 
 
-def Gaussian(x: NDArray, amplitude: float, mean: float, stddev: float) -> NDArray:
+def Gaussian(x: NDArray[floating], amplitude: float, mean: float, stddev: float) -> NDArray[floating]:
     if stddev == 0:
         raise ZeroDivisionError("Standard deviation must be non-zero.")
     return amplitude * exp(-0.5 * ((x - mean) / stddev) ** 2)
 
-def Ratio(var1: NDArray, var2: NDArray) -> NDArray:
+def Ratio(denominator: NDArray[floating], numerator: NDArray[floating]) -> NDArray[floating]:
     '''Berechnet das Verhältnis zweier Variablen var2/var1.'''
-    return var2/var1
+    return numerator/denominator
--- python/tools/python/plot.py
+++ python/tools/python/plot.py
@@ -16,9 +16,9 @@ def DefaultScatter(ax, x, y, title="", label="", xlabel="", ylabel="", c='k'):
     
 def ScatterWithErrorBars(ax, x, y,
                       x_absErr=0, y_absErr=0, title="", label="", xlabel="", ylabel="", data_color="k.", error_color="r", legend_loc='best', grid=True):
-    ax.set_title(title)
-    ax.set_xlabel(xlabel)
-    ax.set_ylabel(ylabel)
+    ax.set_title(title, fontsize = 20)
+    ax.set_xlabel(xlabel, fontsize = 18)
+    ax.set_ylabel(ylabel, fontsize = 18)
     ax.errorbar(x, y, fmt=data_color, xerr=x_absErr, yerr=y_absErr, label=label, ecolor=error_color, capsize=1.5)
     ax.legend(loc=legend_loc)
     ax.grid(visible=grid)
