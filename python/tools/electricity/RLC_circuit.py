f6901c91c231cd0f757410d0ead48eb6b82b80bf
tobi-v <t_vetter@live.de>
2025-12-06 18:58:47 +0100

    Add degree option to RLC circuit phase

--- python/tools/electricity/RLC_circuit.py
+++ python/tools/electricity/RLC_circuit.py
@@ -1,9 +1,13 @@
-from numpy import atan2, sqrt
+from numpy import atan2, pi, sqrt
 from numpy.typing import NDArray
 
 def SeriesAmplitude(omega: NDArray, R: float, L: float, C: float) -> NDArray:
     return sqrt(R**2 + (omega*L - 1/(omega*C))**2)
 
 
-def SeriesPhase(omega: NDArray, R: float, L: float, C: float) -> NDArray:
-    return atan2(omega*L - 1/(omega*C), R)
+def SeriesPhase(omega: NDArray, R: float, L: float, C: float, deg: bool=False) -> NDArray:
+    result = atan2(omega*L - 1/(omega*C), R)
+    if deg:
+        return result * 180/pi
+    else:
+        return result
