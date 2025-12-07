56f4f68cab71ebf22984d98367ea99f20204d457
tobi-v <t_vetter@live.de>
2025-12-07 20:31:18 +0100

    Correction in Stefan Boltzmann for relative resistance

--- python/tools/thermo/stefan_boltzmann.py
+++ python/tools/thermo/stefan_boltzmann.py
@@ -5,4 +5,4 @@ def StefanBoltzmann(delT: NDArray, area: float, T_0: float):
     return sigma*area*((T_0 + delT)**4 - T_0**4)
 
 def StefanBoltzmannRelativeResistance(delR: NDArray, area: float, temp_coeff: float, T_0: float):
-    return sigma*area*((T_0 + temp_coeff*delR)**4 - T_0**4)
+    return sigma*area*((T_0 + delR/temp_coeff)**4 - T_0**4)
