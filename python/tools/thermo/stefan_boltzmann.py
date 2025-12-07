992648a83cd56ffb69061337b1dbe5bcf28d4b62
tobi-v <t_vetter@live.de>
2025-12-07 20:04:12 +0100

    Stefan-Boltzmann for relative resistance

--- python/tools/thermo/stefan_boltzmann.py
+++ python/tools/thermo/stefan_boltzmann.py
@@ -3,3 +3,6 @@ from scipy.constants import Stefan_Boltzmann as sigma
 
 def StefanBoltzmann(delT: NDArray, area: float, T_0: float):
     return sigma*area*((T_0 + delT)**4 - T_0**4)
+
+def StefanBoltzmannRelativeResistance(delR: NDArray, area: float, temp_coeff: float, T_0: float):
+    return sigma*area*((T_0 + temp_coeff*delR)**4 - T_0**4)
