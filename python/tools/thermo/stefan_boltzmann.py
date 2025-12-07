5813ec7d0d5fd53592e1cebd77327db585012127
tobi-v <t_vetter@live.de>
2025-12-07 19:30:13 +0100

    Adjust argument order for Stefan-Boltzmann

--- python/tools/thermo/stefan_boltzmann.py
+++ python/tools/thermo/stefan_boltzmann.py
@@ -1,5 +1,5 @@
 from numpy.typing import NDArray
 from scipy.constants import Stefan_Boltzmann as sigma
 
-def StefanBoltzmann(area: float, T_0: float, delT: NDArray):
+def StefanBoltzmann(delT: NDArray, area: float, T_0: float):
     return sigma*area*((T_0 + delT)**4 - T_0**4)
