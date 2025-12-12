a1308825d00fdebaac5d76ad5dbc3a94e98dc08e
Tobias Vetter <tobias.vetter@dlr.de>
2025-12-12 13:39:23 +0100

    Values

--- python/praktikum/24_Wirbelstrombremse.py
+++ python/praktikum/24_Wirbelstrombremse.py
@@ -1,14 +1,16 @@
-from numpy import sin
+from numpy import arcsin, array
 
 ### 3.1 Vorbereitung
 d_magnet = 0.02
 h_magnet = 0.004
 m_magnet = 0.02805/3
 m_magnet_klein = 0.00168
+m_ausgleich = 0.02951
 m_Auto = 0.02765
-B_magnet = 
+#B_magnet = 
 
 d_blech_cu =
+s_lichtschranken = 0.51
 
 hypothenuse = 0.7 # m
 
@@ -19,4 +21,28 @@ B_max_3 = 0.4
 
 ### 3.3.1
 h_alpha_min = 0.03
-alpha_min = sin^-1
+alpha_min = arcsin(h_alpha_min/hypothenuse)
+t_ungebremst = array([2.496, 2.179, 1.998, 2.11, 2.157, 2.207])  # Ausgleichsgew für 3 Magn
+h = 
+t_ungebremst = array([1.023, 1.006, 1.011, 1.021, 1.022, 1.013])  # Ausgleichsgew für 3 Magn
+h = 0.215
+t_ungebremst = array([0.534, 0.536, 0.537, 0.534, 0.534, 0.537])  # Ausgleichsgew für 3 Magn
+h = 
+t_ungebremst_mittelwinkel = array([])  # Ausgleichsgew für 3 Magn
+h = 
+t_ungebremst = array([])  # Ausgleichsgew für 3 Magn
+h = 
+t_ungebremst = array([])  # Ausgleichsgew für 3 Magn
+t_gebremst = array([])    # 3 Magneten
+
+### 3.3.2 Abhängigkeit von der Blechdicke
+
+t_gebremst_2mm = array([])
+t_gebremst_3mm = array([])
+
+### 3.3.3 Abhängigkeicht von der Feldstärke
+
+t_gebremst_2magnete = array([])
+t_gebremst_1magnet = array([])
+
+### 3.3.4
