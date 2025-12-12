b02e5dadbc8323412762259046f124d8939d64ec
Tobias Vetter <tobias.vetter@dlr.de>
2025-12-12 13:23:03 +0100

    Some values

---
+++ python/praktikum/24_Wirbelstrombremse.py
@@ -0,0 +1,22 @@
+from numpy import sin
+
+### 3.1 Vorbereitung
+d_magnet = 0.02
+h_magnet = 0.004
+m_magnet = 0.02805/3
+m_magnet_klein = 0.00168
+m_Auto = 0.02765
+B_magnet = 
+
+d_blech_cu =
+
+hypothenuse = 0.7 # m
+
+# 3.2 Magnetische Flussdichte
+B_max_1 = 
+B_max_2 = 0.34
+B_max_3 = 0.4
+
+### 3.3.1
+h_alpha_min = 0.03
+alpha_min = sin^-1
