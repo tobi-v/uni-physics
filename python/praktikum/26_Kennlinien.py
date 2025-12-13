760ef5ec1956e8f61a137bf0e180c1ef22bfaa6a
tobi-v <t_vetter@live.de>
2025-12-13 11:58:10 +0100

    Filter trivial value for SB

--- python/praktikum/26_Kennlinien.py
+++ python/praktikum/26_Kennlinien.py
@@ -20,7 +20,9 @@ def Ex01():
     return R/R_0 - 1
 
   def PlotPowerRelativeResistance(ax: plt.Axes, R: NDArray, U:NDArray, I:NDArray, material: str):
-    ax.plot(RelativeResistance(R, R[0]), Power(U, I), '+C0', label=f'Power and Relative Resistance of {material}')
+    print(RelativeResistance(R[1:], R[0]))
+    print(Power(U[1:],I[1:]))
+    ax.plot(RelativeResistance(R[1:], R[0]), Power(U[1:], I[1:]), '+C0', label=f'Power and Relative Resistance of {material}')
     ax.set_xlabel('Relative Resistance r')
     ax.set_ylabel('Power (W)')
     ax.set_title(f'Power over Relative Resistance {material}')
@@ -30,6 +32,21 @@ def Ex01():
     ax.legend()
     plt.tight_layout()
 
+  def PlotBoltzi(ax: plt.Axes, R: NDArray, U: NDArray, I: NDArray, material: str, guess: list):
+    PlotPowerRelativeResistance(ax, R, U, I, material)
+    popt, pcov = curve_fit(StefanBoltzmannRelativeResistance, RelativeResistance(R[1:], R[0]), Power(U[1:], I[1:]), p0=guess, bounds=([0, -1, 290], [0.1, 1, 400]))
+    area, temp_coeff, T_0 = popt
+    p_uncertainties = sqrt(diag(pcov))
+    area_uncertainty, temp_coeff_uncertainty, T_0_uncertainty = p_uncertainties
+    print(f"\n--- Params for {material} ---\n" \
+          f"Area:\t\t\t\t{area} +/- {area_uncertainty:.3g} m,\n" \
+          f"Temperature Coefficient:\t{temp_coeff} +/- {temp_coeff_uncertainty:.3g} 1/K,\n" \
+          f"Start temperature:\t\t{T_0} +/- {T_0_uncertainty:.3g} K")
+    ax.plot(RelativeResistance(R[1:], R[0]), StefanBoltzmannRelativeResistance(RelativeResistance(R[1:], R[0]), *popt), '--k', label='Fit: Stefan-Boltzmann Law')
+    ax.plot(RelativeResistance(R[1:], R[0]), StefanBoltzmannRelativeResistance(RelativeResistance(R[1:], R[0]), *(popt + p_uncertainties)), ':r', label='Fit Uncertainty')
+    ax.plot(RelativeResistance(R[1:], R[0]), StefanBoltzmannRelativeResistance(RelativeResistance(R[1:], R[0]), *(popt - p_uncertainties)), ':r')
+    ax.legend()    
+
   # Messdaten
   V = linspace(2, 40, 20)
   U_tungsten = array([2.05, 3.97, 5.93, 7.96, 9.91, 11.82, 13.78, 15.74, 17.71, 19.65, 21.5, 23.5, 25.4, 27.4, 29.3, 31.3, 33.2, 35.2, 37.1, 39.0])
@@ -37,9 +54,13 @@ def Ex01():
   R_tungsten = U_tungsten/I_tungsten
   U_carbon = array([1.9, 4.0, 5.9, 7.8, 9.8, 11.8, 13.7, 15.6, 17.6, 19.5, 21.5, 23.5, 25.4, 27.4, 29.3, 31.2, 33.2, 35.1, 37.1, 39.1])
   I_carbon = array([1.1, 2.1, 3.2, 4.2, 5.4, 6.4, 7.6, 8.7, 9.8, 11.0, 12.2, 13.4, 14.6, 15.8, 17.0, 18.2, 19.5, 20.7, 22.0, 23.3])*10**-3 #A
-  R_carbon = U_carbon/I_carbon
+  R_carbon = U_carbon/I_carbon  
+  U_carbon_filtered = array([1.9, 4.0, 7.8, 9.8, 13.7, 15.6, 19.5, 21.5, 23.5, 25.4, 27.4, 29.3, 31.2, 33.2, 35.1, 37.1, 39.1])
+  I_carbon_filtered = array([1.1, 2.1, 4.2, 5.4, 7.6, 8.7, 11.0, 12.2, 13.4, 14.6, 15.8, 17.0, 18.2, 19.5, 20.7, 22.0, 23.3])*10**-3 #A
+  R_carbon_filtered = U_carbon_filtered/I_carbon_filtered
   # Pyrometer: 750°C  bei 54.2 V, 28.6 mA
   CheckLengths(V, U_tungsten, U_carbon, I_tungsten, I_carbon)
+  CheckLengths(U_carbon_filtered, I_carbon_filtered, R_carbon_filtered)
 
   # UI-Plots
   fig1, axs = plt.subplots(2, 1)
@@ -52,20 +73,10 @@ def Ex01():
   initial_guesses = [[0.001, 0.003, 293], [0.001, -0.003, 293]]
   fig2, axs = plt.subplots(2, 1)
   for ax, R, U, I, material, guess in zip(axs, [R_tungsten, R_carbon], [U_tungsten, U_carbon], [I_tungsten, I_carbon], ['Tungsten', 'Carbon'], initial_guesses):
-    PlotPowerRelativeResistance(ax, R, U, I, material)
-    popt, pcov = curve_fit(StefanBoltzmannRelativeResistance, RelativeResistance(R, R[0]), Power(U, I), p0=guess, bounds=([0, -1, 290], [0.1, 1, 400]))
-    area, temp_coeff, T_0 = popt
-    p_uncertainties = sqrt(diag(pcov))
-    area_uncertainty, temp_coeff_uncertainty, T_0_uncertainty = p_uncertainties
-    print(f"\n--- Params for {material} ---\n" \
-          f"Area:\t\t\t\t{area} +/- {area_uncertainty:.3g} m,\n" \
-          f"Temperature Coefficient:\t{temp_coeff} +/- {temp_coeff_uncertainty:.3g} 1/K,\n" \
-          f"Start temperature:\t\t{T_0} +/- {T_0_uncertainty:.3g} K")
-    ax.plot(RelativeResistance(R, R[0]), StefanBoltzmannRelativeResistance(RelativeResistance(R, R[0]), *popt), '--k', label='Fit: Stefan-Boltzmann Law')
-    ax.plot(RelativeResistance(R, R[0]), StefanBoltzmannRelativeResistance(RelativeResistance(R, R[0]), *(popt + p_uncertainties)), ':r', label='Fit Uncertainty')
-    ax.plot(RelativeResistance(R, R[0]), StefanBoltzmannRelativeResistance(RelativeResistance(R, R[0]), *(popt - p_uncertainties)), ':r')
-    ax.legend()
+    PlotBoltzi(ax, R, U, I, material, guess)
   #plt.close(fig2)
+  fig3, ax = plt.subplots()
+  PlotBoltzi(ax, R_carbon_filtered, U_carbon_filtered, I_carbon_filtered, 'Filtered Carbon Data', initial_guesses[1])
 
 def Ex02():
   U1 = array([2.0, 4.0, 4.9, 5.1, 5.2, 5.3, 5.4, 5.5, 5.6, 5.7, 5.8, 5.9])
