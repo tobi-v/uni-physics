99a87fe98980f9814da9afb65bca5068f051a42d
tobi-v <t_vetter@live.de>
2025-12-27 13:29:13 +0100

    Restructuring. Calculate angles and times.

--- python/praktikum/24_Wirbelstrombremse.py
+++ python/praktikum/24_Wirbelstrombremse.py
@@ -1,4 +1,9 @@
 from numpy import arcsin, array, inf
+from tools.statistics.uncertainty_calculation import GetResultAndUncertainty, MeanAndStd
+
+waage_uncertainty = 1e-5
+ruler_uncertainty = 2e-3
+caliper_uncertainty = 1e-4
 
 ### 3.1 Vorbereitung
 d_magnet = 0.02
@@ -7,7 +12,6 @@ m_magnet = 0.02805/3
 m_magnet_klein = 0.00168
 m_ausgleich = 0.02951
 m_Auto = 0.02765
-#B_magnet = 
 B_uncertainty = 0.01
 
 s_lichtschranken = 0.51
@@ -19,33 +23,48 @@ B_max_2 = 0.34
 B_max_3 = 0.4
 
 ### 3.3.1
-h_alpha_min = 0.03
-alpha_min = arcsin(h_alpha_min/hypothenuse)
-t_ungebremst = array([2.496, 2.179, 1.998, 2.11, 2.157, 2.207])  # Ausgleichsgew für 3 Magn
-t_gebremst = array([inf, inf, inf, inf, inf, inf])    # 3 Magneten
-h = 0.08
-t_ungebremst = array([1.023, 1.006, 1.011, 1.021, 1.022, 1.013])  # Ausgleichsgew für 3 Magn
-t_gebremst = array([14.36, 14.48, 14.39, 14.52, 14.39, 14.59])    # 3 Magneten
-h = 0.215
-t_ungebremst = array([0.534, 0.536, 0.537, 0.534, 0.534, 0.537])  # Ausgleichsgew für 3 Magn
-t_gebremst = array([4.153, 4.109, 4.117, 4.096, 4.115, 4.12])    # 3 Magneten
-h = 0.293
-t_ungebremst_mittelwinkel = array([0.441, 0.438, 0.440, 0.438, 0.441, 0.440])  # Ausgleichsgew für 3 Magn
-t_gebremst = array([2.941, 2.897, 2.943, 2.939, 2.940, 2.903])    # 3 Magneten
-h = 0.36
-t_ungebremst = array([0.393, 0.395, 0.397, 0.395, 0.393, 0.392])  # Ausgleichsgew für 3 Magn
-t_gebremst = array([2.316, 2.309, 2.304, 2.29, 2.285, 2.282])    # 3 Magneten
-h = 0.43
-t_ungebremst = array([0.352, 0.358, 0.352, 0.361, 0.35, 0.351])  # Ausgleichsgew für 3 Magn
-t_gebremst = array([1.914, 1.897, 1.899, 1.892, 1.904, 1.9])    # 3 Magneten
+def Ex_3_3_1():
+    def AngleFromHeight(h, hypothenuse):
+        return arcsin(h/(hypothenuse+1e-10))
+
+    h_min = 0.03
+    alpha_min_mean, alpha_min_uncertainty = GetResultAndUncertainty(AngleFromHeight, [h_min, hypothenuse], True, [ruler_uncertainty, ruler_uncertainty])
+    print(f"Minimalwinkel in Grad: {alpha_min_mean*180/3.1416:.2f} +/- {alpha_min_uncertainty*180/3.1416:.2f}")
+    t_ungebremst = array([2.496, 2.179, 1.998, 2.11, 2.157, 2.207])  # Ausgleichsgew für 3 Magn
+    t_gebremst = array([inf, inf, inf, inf, inf, inf])    # 3 Magneten
+    h = 0.08
+    t_ungebremst = array([1.023, 1.006, 1.011, 1.021, 1.022, 1.013])  # Ausgleichsgew für 3 Magn
+    t_gebremst = array([14.36, 14.48, 14.39, 14.52, 14.39, 14.59])    # 3 Magneten
+    h = 0.215
+    t_ungebremst = array([0.534, 0.536, 0.537, 0.534, 0.534, 0.537])  # Ausgleichsgew für 3 Magn
+    t_gebremst = array([4.153, 4.109, 4.117, 4.096, 4.115, 4.12])    # 3 Magneten
+    h_mid = 0.293
+    # Produces nan for default scipy.derivative -> adjust initial_step
+    alpha_mid_mean, alpha_mid_uncertainty = GetResultAndUncertainty(AngleFromHeight, [h_mid, hypothenuse], True, [ruler_uncertainty, ruler_uncertainty])
+    print(f"Mid angle in degree: {alpha_mid_mean*180/3.1416:.2f} +/- {alpha_mid_uncertainty*180/3.1416:.2f}")
+    t_ungebremst_mid = array([0.441, 0.438, 0.440, 0.438, 0.441, 0.440])  # Ausgleichsgew für 3 Magn
+    t_gebremst_mid = array([2.941, 2.897, 2.943, 2.939, 2.940, 2.903])    # 3 Magneten
+    h = 0.36
+    t_ungebremst = array([0.393, 0.395, 0.397, 0.395, 0.393, 0.392])  # Ausgleichsgew für 3 Magn
+    t_gebremst = array([2.316, 2.309, 2.304, 2.29, 2.285, 2.282])    # 3 Magneten
+    h = 0.43
+    t_ungebremst = array([0.352, 0.358, 0.352, 0.361, 0.35, 0.351])  # Ausgleichsgew für 3 Magn
+    t_gebremst = array([1.914, 1.897, 1.899, 1.892, 1.904, 1.9])    # 3 Magneten
 
 ### 3.3.2 Abhängigkeit von der Blechdicke
 
-d_duenn = 0.001
-d_mittel = 0.002
-d_dick = 0.003
-t_gebremst_2mm = array([4.519, 4.481, 4.48, 4.493, 4.492, 4.490])
-t_gebremst_3mm = array([5.8, 5.762, 5.755, 5.755, 5.781, 5.779])
+def Ex_3_3_2():
+    d_duenn = 0.001
+    d_mittel = 0.002
+    d_dick = 0.003
+    t_gebremst_thin = array([2.941, 2.897, 2.943, 2.939, 2.940, 2.903])
+    t_gebremst_mid = array([4.519, 4.481, 4.48, 4.493, 4.492, 4.490])
+    t_gebremst_thicc = array([5.8, 5.762, 5.755, 5.755, 5.781, 5.779])
+
+    for t, thickness in zip([t_gebremst_thin, t_gebremst_mid, t_gebremst_thicc], ["thin", "medium", "thicc"]):
+        t_mean, t__uncertainty = MeanAndStd(t)
+        print(f"Thickness: {thickness}, t = {t_mean:.3f} +/- {t__uncertainty:.3f}")
+
 
 ### 3.3.3 Abhängigkeicht von der Feldstärke
 
@@ -70,3 +89,8 @@ t_Stahl = array([0.479, 0.481, 0.482, 0.486, 0.477, 0.482])
 
 d_lichtschranken = 0.12
 t = array([0.757, 0.75, 0.75, 0.755, 0.751, 0.75])
+
+###
+
+Ex_3_3_1()
+Ex_3_3_2()
