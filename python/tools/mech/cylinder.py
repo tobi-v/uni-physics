9970e8207d47310d8a1d3520cccb8cf4525cfbe0
tobi-v <t_vetter@live.de>
2026-01-17 09:21:10 +0100

    More cylinder calculations

--- python/tools/mech/cylinder.py
+++ python/tools/mech/cylinder.py
@@ -1,10 +1,17 @@
-import numpy as np
+from numpy import pi
 from tools.statistics.uncertainty_calculation import GetResultAndUncertainty
 
+def VolumeOfCylinder(length, radius, uncertainty=False, delL=0, delR=0):
+    def VolumeOfCylinderInner(length, radius):
+        return pi * length * radius**2
+
+    return GetResultAndUncertainty(
+        VolumeOfCylinderInner, [length, radius], uncertainty, [delL, delR]
+    )
 
 def MassFromVolume(length, radius, rho, uncertainty=False, delL=0, delR=0, delRho=0):
     def MassFromVolumeInner(length, radius, rho):
-        return rho * np.pi * length * radius**2
+        return rho * pi * length * radius**2
 
     return GetResultAndUncertainty(
         MassFromVolumeInner, [length, radius, rho], uncertainty, [delL, delR, delRho]
