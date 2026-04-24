5bfdc99a446a80addcc19bcf381aafb4ec8f13ac
Tobias Vetter <tobias.vetter@dlr.de>
2026-04-24 14:34:31 +0200

    Print angles and wavelength for unknown lamp

--- python/praktikum/20_Spektroskopie.py
+++ python/praktikum/20_Spektroskopie.py
@@ -21,6 +21,7 @@ def Hg(α_0: float, hg_λ: NDArray[float64], hg_δmin_III: NDArray[float64], hg_
     return Polyreg(α_0 - hg_δmin_III, 1/hg_λ, order=3)
 
 def He(mapping, he_δmin: NDArray[float64]):
+    print("=== δmin to λ for He ===")
     δmin_to_f, _, δmin_to_f_cov = mapping
     δmin = α_0 - he_δmin
     print(f"δmin: {δmin} +/- {angle_uncertainty:.3f}")
@@ -28,8 +29,14 @@ def He(mapping, he_δmin: NDArray[float64]):
     he_λ = 1/δmin_to_f(δmin)
     print(f"λ: {he_λ} +/- {(1/(δmin_to_f_cov[0][0]**0.5) * he_δmin)} nm")
 
-def Unbekannt():
-    pass
+def Unknown(mapping, x_δmin: NDArray[float64]):
+    print("=== δmin to λ for Unknown lamp ===")
+    δmin_to_f, _, δmin_to_f_cov = mapping
+    δmin = α_0 - x_δmin
+    print(f"δmin: {δmin} +/- {angle_uncertainty:.3f}")
+
+    he_λ = 1/δmin_to_f(δmin)
+    print(f"λ: {he_λ} +/- {(1/(δmin_to_f_cov[0][0]**0.5) * x_δmin)} nm")
 
 α_0 = 259.4
 α_0 = dms_to_rad(α_0)
@@ -42,16 +49,17 @@ hg_δmin_II = data['II'].to_numpy()
 hg_δmin_II = array([dms_to_rad(angle) for angle in hg_δmin_II])
 hg_δmin_I = data['I'].to_numpy()
 hg_δmin_I = array([dms_to_rad(angle) for angle in hg_δmin_I])
-mapping = Hg(α_0, hg_λ, hg_δmin_III, hg_δmin_II, hg_δmin_I)
+mapping_fun = Hg(α_0, hg_λ, hg_δmin_III, hg_δmin_II, hg_δmin_I)
 
 he_color = data['He III'].iloc[0:6].to_numpy()
 he_δmin = data['Winkel_He'].iloc[0:6].to_numpy()
 he_δmin = array([dms_to_rad(angle) for angle in he_δmin])
-He(mapping, he_δmin)
+He(mapping_fun, he_δmin)
 
+print("")
 unb_color = data['Unbekannt III'].iloc[0:6].to_numpy()
-unb_δmin = data['Winkel_Unb'].iloc[0:6].to_numpy()
-unb_δmin = array([dms_to_rad(angle) for angle in unb_δmin])
-Unbekannt()
+x_δmin = data['Winkel_Unb'].iloc[0:6].to_numpy()
+x_δmin = array([dms_to_rad(angle) for angle in x_δmin])
+Unknown(mapping_fun, x_δmin)
 
 plt.show()
