c7d5144e330f4557987b8dc8a77359cbf8a0a221
tobi-v <t_vetter@live.de>
2026-05-10 11:59:01 +0200

    Animation of harmonic quantum oscillator

--- python/atm/0405_harmonic_quantum_oscillator.py
+++ python/atm/0405_harmonic_quantum_oscillator.py
@@ -1,20 +1,38 @@
-import matplotlib.pyplot as plt
 from numpy import abs, exp, linspace, pi, sqrt
-from scipy.constants import hbar
+from matplotlib import pyplot as plt
+from matplotlib.animation import FuncAnimation
+
+m = 1.0
+ω = 1.0
+hbar = 1.0
+
+x = linspace(-5, 5, 1000)
+
+def 𝛙_0(x):
+    return (m*ω / (pi*hbar))**(1/4) * exp(-m*ω*x**2 / (2*hbar))
+
+def 𝛙_1(x):
+    return (m*ω / (pi*hbar))**(1/4) * sqrt(2*m*ω / hbar)*x * exp(-m*ω*x**2 / (2*hbar))
 
 def 𝛙(x, t):
-    m = 1
-    ω = 1
-    val = exp(-3j*ω*t/2)
-    val = sqrt(2*m*ω/hbar) * val
-    val = exp(-1j*ω*t/2) + val
-    val = exp(-m*ω*x/(2*hbar)) * val
-    val = (m*ω/(hbar*pi))**(1/4) * val
-    return val
+    return 𝛙_0(x)*exp(-1j*ω / 2*t) + 𝛙_1(x)*exp(-1j*3*ω / 2*t)
 
-x = linspace(-1, 1, 1000)
+def prob_density(x, t):
+    return abs(𝛙(x, t))**2
 
 fig, ax = plt.subplots()
-ax.plot(x, abs(𝛙(x, 1))**2)
+ax.set_xlabel('Position x')
+ax.set_ylabel('$|\\psi(x,t)|^2$')
+ax.set_title('Zeitliche Veränderung der Wahrscheinlichkeitsdichte')
 
+line, = ax.plot(x, prob_density(x, 0), lw=2)
+
+def update(t):
+    line.set_ydata(prob_density(x, t))
+    ax.set_title(f'Zeitliche Veränderung der Wahrscheinlichkeitsdichte (t = {t:.2f})')
+    return line,
+
+ani = FuncAnimation(fig, update, frames=linspace(0, 2*pi/ω, 100), interval=50, blit=True)
+
+plt.grid(True)
 plt.show()
