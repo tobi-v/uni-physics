6ee07f388dee9e5f86b89fdf526dc5b342f06649
tobi-v <t_vetter@live.de>
2026-05-10 18:27:00 +0200

    Calculate steam lamp wavelength

--- python/praktikum/19_michelson.py
+++ python/praktikum/19_michelson.py
@@ -8,15 +8,28 @@ def κ_fun(N, λ, Δx, uncertainty=False, delN=0, delλ=0, delΔx=0):
     
     return GetResultAndUncertainty(κ_inner, [N, λ, Δx], uncertainty, [delN, delλ, delΔx])
 
+def λ_fun(N, κ, Δx, uncertainty=False, delN=0, delκ=0, delΔx=0):
+    def λ_inner(N, κ, Δx):
+        return 2*κ*Δx/N
+    
+    return GetResultAndUncertainty(λ_inner, [N, κ, Δx], uncertainty, [delN, delκ, delΔx])
+
 N = 200
 N_σ = 10
-xs = [0.93, 0.995, 0.855]   # [mm]
-Δx_μ = mean(xs)
-Δx_σ = std(xs)
-λ = 532e-6  # [mm]
+Δx = [0.93, 0.995, 0.855]   # [mm]
+Δx_μ = mean(Δx)
+Δx_σ = std(Δx)
+λ_laser = 532 # [nm]
 
 print(f"Δs: {Δx_μ:.3f} ± {Δx_σ:.3f} mm")
 
-κ, κ_σ = κ_fun(N, λ, Δx_μ, uncertainty=True, delN=N_σ, delλ=0, delΔx=Δx_σ)
+κ, κ_σ = κ_fun(N, λ_laser, Δx_μ, uncertainty=True, delN=N_σ, delλ=0, delΔx=Δx_σ)
 
 print(f"κ: {κ:.4f} ± {κ_σ:.4f}")
+
+Δx_dampflampe = [0.96, 0.99, 0.97]   # [mm]
+Δx_dampflampe_μ = mean(Δx_dampflampe)
+Δx_dampflampe_σ = std(Δx_dampflampe)
+λ_dampflampe, λ_dampflampe_σ = λ_fun(N, κ, Δx_dampflampe_μ, uncertainty=True, delN=N_σ, delκ=κ_σ, delΔx=Δx_dampflampe_σ)
+
+print(f"λ_dampflampe: {λ_dampflampe:.4f} ± {λ_dampflampe_σ:.4f} nm")
