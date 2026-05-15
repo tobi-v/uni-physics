cf5a27a7ad1f03e79e30e50186b97fc0a6d11c92
Tobias Vetter <tobias.vetter@dlr.de>
2026-05-15 15:12:32 +0200

    WIP

---
+++ python/praktikum/15_geometr_optik.py
@@ -0,0 +1,13 @@
+def f_abbildungsverf(b, g):
+    return 1 / (1 / b + 1 / g)
+
+def f_comb(f1, f2, d):
+    return 1 / (1 / f1 + 1 / f2 - d / (f1 * f2))
+
+def d_from_f(f, f1, f2):
+    return (1 / f1 + 1 / f2 - 1 / f) * (f1 * f2)
+
+def Abbildungsverfahren():
+    f = 
+
+Abbildungsverfahren()
