888ea5556302f943255a41ab97038fb9a5d48260
tobi-v <t_vetter@live.de>
2026-05-31 12:39:08 +0200

    Plot Fourier Trafo

--- python/dda/01_rlc/signal_examination.py
+++ python/dda/01_rlc/signal_examination.py
@@ -1,6 +1,7 @@
 from matplotlib import pyplot as plt
 from os.path import dirname, join
 from pandas import read_csv
+from tools.signals.fourier import fft
 
 def load_data(file):
     script_dir = dirname(__file__)
@@ -12,7 +13,14 @@ def plot_signals():
     capacitances = [100, 150, 220]  # [mF]
     frequencies = [500, 1000, 2000] # [Hz]
 
-    fig, axs = plt.subplots(6, 3)
+    _, t_axs = plt.subplots(6, 3)
+    _, ft_axs = plt.subplots(6,3)
+
+    def plot(ax, channel, C, f, x, y, x_label):
+        ax.plot(x, y)
+        ax.set_xlabel(x_label)
+        ax.set_ylabel('Amplitude [V]')
+        ax.set_title(f'C = {C} mF, f = {f} Hz, Channel {channel}')
 
     for ii, C in enumerate(capacitances):
         for jj, f in enumerate(frequencies):
@@ -22,17 +30,16 @@ def plot_signals():
             ch1 = ch1[mask]
             ch2 = ch2[mask]
 
-            axs[2*ii][jj].plot(t, ch1)
-            axs[2*ii][jj].set_xlabel('Zeit [s]')
-            axs[2*ii][jj].set_ylabel('Amplitude [V]')
-            axs[2*ii][jj].set_title(f'C = {C} mF, f = {f} Hz, Channel 1')
-            axs[2*ii+1][jj].plot(t, ch2)
-            axs[2*ii+1][jj].set_xlabel('Zeit [s]')
-            axs[2*ii+1][jj].set_ylabel('Amplitude [V]')
-            axs[2*ii+1][jj].set_title(f'C = {C} mF, f = {f} Hz, Channel 2')
+            plot(t_axs[2*ii][jj], 1, C, f, t, ch1, x_label="Zeit [s]")
+            plot(t_axs[2*ii+1][jj], 2, C, f, t, ch2, x_label="Zeit [s]")
+            plt.subplots_adjust(hspace=0.9)
 
-    plt.subplots_adjust(hspace=0.9)
-    plt.tight_layout()
-    plt.show()
+            _, ch1_ft = fft(t, ch1)
+            freqs, ch2_ft = fft(t, ch2)
+            
+            plot(ft_axs[2*ii][jj], 1, C, f, freqs, ch1_ft, x_label="Frequenz [s]")
+            plot(ft_axs[2*ii+1][jj], 2, C, f, freqs, ch2_ft, x_label="Frequenz [s]")
+            plt.subplots_adjust(hspace=0.9)
 
 plot_signals()
+plt.show()
