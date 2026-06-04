5bd06a91626d9f268dc8fac7998aa59416225528
tobi-v <t_vetter@live.de>
2026-06-04 14:28:30 +0200

    Add function for calculating the amplitude at the resistor of an RLC circuit

--- python/tools/electricity/RLC_circuit.py
+++ python/tools/electricity/RLC_circuit.py
@@ -1,13 +1,22 @@
 from numpy import atan2, pi, sqrt
 from numpy.typing import NDArray
+from tools.statistics.uncertainty_calculation import GetResultAndUncertainty
 
-def SeriesAmplitude(omega: NDArray, R: float, L: float, C: float) -> NDArray:
-    return sqrt(R**2 + (omega*L - 1/(omega*C))**2)
+def SeriesAmplitude(ω: NDArray, R: float, L: float, C: float) -> NDArray:
+    # TODO: Specify which amplitude is calculated here and enable uncertainty calculation
+    return sqrt(R**2 + (ω*L - 1/(ω*C))**2)
 
 
-def SeriesPhase(omega: NDArray, R: float, L: float, C: float, deg: bool=False) -> NDArray:
-    result = atan2(omega*L - 1/(omega*C), R)
+def SeriesPhase(ω: NDArray, R: float, L: float, C: float, deg: bool=False) -> NDArray:
+    # TODO: Specify which phase is calculated here and enable uncertainty calculation
+    result = atan2(ω*L - 1/(ω*C), R)
     if deg:
         return result * 180/pi
     else:
         return result
+    
+def SeriesAmplitudeAtR(ω: NDArray, R: float, L: float, C: float, uncertainty=False, Δω=0, ΔR=0, ΔL=0, ΔC=0):
+    def SeriesAmplitudeAtRInner(ω: NDArray, R: float, L: float, C: float):
+        return sqrt(R**2 / (R**2 + (ω*L - 1/(ω*C))**2) )
+
+    return GetResultAndUncertainty(SeriesAmplitudeAtRInner, [ω, R, L, C], uncertainty, [Δω, ΔR, ΔL, ΔC])
