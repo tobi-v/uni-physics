0a827164e5b6fbd1a61050f6b9b47052cb5ae96c
tobi-v <t_vetter@live.de>
2026-06-06 23:12:28 +0200

    WIP

---
+++ python/dda/02_2dstoss/impulse_examination.py
@@ -0,0 +1,12 @@
+from os.path import dirname, join
+from pandas import read_csv
+
+def read(file):
+    script_dir = dirname(__file__)
+    csv_path = join(script_dir, file)
+    data = read_csv(csv_path)
+    return data
+
+def Ex_32_01_01():
+    
+    pass
