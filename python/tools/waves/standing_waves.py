9785d5edb6d5db88f0c78f5bfbac8b9f6a0013b7
tobi-v <t_vetter@live.de>
2026-06-11 22:31:46 +0200

    Make sure uncertainties in standing wave calculation have the same length

--- python/tools/waves/standing_waves.py
+++ python/tools/waves/standing_waves.py
@@ -1,3 +1,4 @@
+from numpy import zeros_like
 from tools.statistics.uncertainty_calculation import GetResultAndUncertainty
 
 def λ_caseA(length, order, uncertainty=False, Δlength=0):
@@ -5,11 +6,11 @@ def λ_caseA(length, order, uncertainty=False, Δlength=0):
     def λ_caseAInner(length, order):
         return 2*length / (2*order - 1)
     
-    return GetResultAndUncertainty(λ_caseAInner, [length, order], uncertainty, [Δlength, 0])
+    return GetResultAndUncertainty(λ_caseAInner, [length, order], uncertainty, [Δlength, zeros_like(Δlength)])
 
 def λ_caseB(length, order, uncertainty=False, Δlength=0):
     '''Stick clamped at 1/4 and 3/4'''
     def λ_caseBInner(length, order):
         return length / (2*order-1)
     
-    return GetResultAndUncertainty(λ_caseBInner, [length, order], uncertainty, [Δlength, 0])
+    return GetResultAndUncertainty(λ_caseBInner, [length, order], uncertainty, [Δlength, zeros_like(Δlength)])
