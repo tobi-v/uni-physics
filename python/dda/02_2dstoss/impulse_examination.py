97dca71bf8de11a30429004dc56680b33a4ae8d6
tobi-v <t_vetter@live.de>
2026-06-11 17:42:19 +0200

    Adding measurements

--- python/dda/02_2dstoss/impulse_examination.py
+++ python/dda/02_2dstoss/impulse_examination.py
@@ -1,6 +1,12 @@
 from os.path import dirname, join
 from pandas import read_csv
 
+m_green = 18.42e-3  # [kg]
+m_red = 18.46e-3
+m_red_smol = 9.32e-3
+m_bridge = 37.2e-3
+Δm = 0.01e-3
+
 def read(file):
     script_dir = dirname(__file__)
     csv_path = join(script_dir, file)
@@ -8,5 +14,7 @@ def read(file):
     return data
 
 def Ex_32_01_01():
-    
-    pass
+    data_green = read('32_01_01_massGreen.csv')
+    data_red = read('32_01_01_massRed.csv')
+
+Ex_32_01_01()
