4ae589a4e1b74e3d903b080bcf06dac70ecc9608
tobi-v <t_vetter@live.de>
2026-06-14 16:45:21 +0200

    Calculate momentum and plot position for reflected puck. Some data correction.

--- python/dda/02_2dstoss/impulse_examination.py
+++ python/dda/02_2dstoss/impulse_examination.py
@@ -1,3 +1,4 @@
+from matplotlib import pyplot as plt
 from os.path import dirname, join
 from pandas import read_csv
 from tools.files.conversion import save_pandas_to_latex_table
@@ -9,7 +10,7 @@ m_red_smol = 9.32e-3
 m_bridge = 37.2e-3
 Δm = 0.01e-3
 
-header = [
+header01 = [
     r'$t / \si{\second}$',
     r'$p_{x}^{\text{green}}$',
     r'$p_{y}^{\text{green}}$',
@@ -19,6 +20,13 @@ header = [
     r'$p_{y}^{\text{tot}}$',
     r'$|p|$'
 ]
+header02 = [
+    r'$t / \si{\second}$',
+    r'$p_x$',
+    r'$p_y$',
+    r'$|p|$',
+    r'$\theta / \degree$'
+]
 
 def read(file):
     script_dir = dirname(__file__)
@@ -26,35 +34,45 @@ def read(file):
     data = read_csv(csv_path)
     return data
 
+def compute_impulses(data, mass):        
+    def row_impulse(r):
+        p_x, p_y = impulse2D(mass, r.vx, r.vy)
+        p_tot = total_impulse2D(p_x, p_y)
+        return p_x, p_y, p_tot
+
+    data[['p_x', 'p_y', 'p_tot']] = data.apply(lambda r: row_impulse(r), axis=1, result_type='expand')
+    return data
+
 def Impulses(experiment, red_smol=False):
     data_green = read(experiment + '_massGreen.csv')
     data_red = read(experiment + '_massRed.csv')
+    
+    def plotxy(data_green, data_red, experiment):
+        _, ax = plt.subplots()
+        ax.scatter(data_green.x*100, data_green.y*100, c='g', marker='x', label="Mittelpunkt Grüner Puck")
+        ax.scatter(data_red.x*100, data_red.y*100, c='r', marker='x', label="Mittelpunkt Roter Puck")
+        ax.legend()
+        ax.set_xlabel("x [cm]")
+        ax.set_ylabel("y [cm]")
+        ax.grid(visible=True)
+        ax.set_title(f"Positionen der Pucks für Versuch {experiment}")
 
-    def compute_impulses(df, mass):        
-        def row_impulse(r):
-            p_x, p_y = impulse2D(mass, r.vx, r.vy)
-            p_tot = total_impulse2D(p_x, p_y)
-            return p_x, p_y, p_tot
-
-        df[['p_x', 'p_y', 'p_tot']] = df.apply(lambda r: row_impulse(r), axis=1, result_type='expand')
-        return df
-
-    dg = compute_impulses(data_green, m_green)
+    impulses_green = compute_impulses(data_green, m_green)
     if red_smol:
-        dr = compute_impulses(data_red, m_red_smol)
+        impulses_red = compute_impulses(data_red, m_red_smol)
+        m_total = m_green + m_red_smol
     else:
-        dr = compute_impulses(data_red, m_red)
+        impulses_red = compute_impulses(data_red, m_red)
+        m_total = m_green + m_red
 
     # combine impulses for each shared timestamp
-    combined = dg[['t', 'p_x', 'p_y']].copy()
+    combined = impulses_green[['t', 'p_x', 'p_y']].copy()
     combined = combined.rename(columns={'p_x': 'p_x_green', 'p_y': 'p_y_green'})
-    combined['p_x_red'] = dr['p_x'].values
-    combined['p_y_red'] = dr['p_y'].values
+    combined['p_x_red'] = impulses_red['p_x'].values
+    combined['p_y_red'] = impulses_red['p_y'].values
     combined['p_x_total'] = combined['p_x_green'] + combined['p_x_red']
     combined['p_y_total'] = combined['p_y_green'] + combined['p_y_red']
-    combined['p_total'] = combined.apply(
-        lambda r: total_impulse2D(r.p_x_total, r.p_y_total), axis=1
-    )
+    combined['p_total'] = combined.apply(lambda r: total_impulse2D(r.p_x_total, r.p_y_total), axis=1)
 
     # multiply all impulse columns by 1000 (convert from kg·m/s to g·m/s) for nicer display
     combined.iloc[:, 1:] *= 1000
@@ -63,14 +81,50 @@ def Impulses(experiment, red_smol=False):
                                join(dirname(__file__), experiment + '_out_table.txt'),
                                r'Impulse des grünen und des roten Pucks, sowie der gesamte Impuls. Alle Impulsangaben in $\si{\gram\meter\per\second}$.',
                                'tab:'+experiment,
-                               header)
-    return
+                               header01)
+    
+    plotxy(data_green, data_red, experiment)
 
-Impulses('32_01_01')
-Impulses('32_01_02')
-Impulses('32_02_01')
+def SingleImpulse(experiment):# Uses the green puck
+    data = read(experiment + ".csv")
+    
+    def plotxy(data, experiment):
+        _, ax = plt.subplots()
+        ax.scatter(data.x*100, data.y*100, c='r', marker='x', label="Mittelpunkt Puck")
+        ax.legend()
+        ax.set_xlabel("x [cm]")
+        ax.set_ylabel("y [cm]")
+        ax.grid(visible=True)
+        ax.set_title(f"Positionen des Pucks für Versuch {experiment}")
+
+    impulses = compute_impulses(data, m_green)
+
+    # combine impulses for each shared timestamp
+    combined = impulses[['t', 'p_x', 'p_y']].copy()
+    combined['p_total'] = combined.apply(lambda r: total_impulse2D(r.p_x, r.p_y), axis=1)
+    combined['θr'] = data['θr'].values
+
+    # multiply all impulse columns by 1000 (convert from kg·m/s to g·m/s) for nicer display
+    combined.iloc[:, 1:] *= 1000
+
+    save_pandas_to_latex_table(combined,
+                               join(dirname(__file__), experiment + '_out_table.txt'),
+                               r'Impulse des an der Bande reflektierten Pucks. Alle Impulsangaben in $\si{\gram\meter\per\second}$.',
+                               'tab:'+experiment,
+                               header02)
+    
+    plotxy(data, experiment)
+
+# Impulses('32_01_01')
+# Impulses('32_01_02')
+# Impulses('32_02_01')
 Impulses('32_02_02')
-Impulses('33_01_01', red_smol=True)
-Impulses('33_01_02', red_smol=True)
-Impulses('33_02_01', red_smol=True)
-Impulses('33_02_02', red_smol=True)
+# Impulses('33_01_01', red_smol=True)
+# Impulses('33_01_02', red_smol=True)
+# Impulses('33_02_01', red_smol=True)
+# Impulses('33_02_02', red_smol=True)
+
+SingleImpulse('34_01')
+SingleImpulse('34_02')
+
+plt.show()
