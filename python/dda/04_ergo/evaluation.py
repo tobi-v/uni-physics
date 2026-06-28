a0ecfd1beb72f2eb9a8b0b975a7eb8bd28749de8
tobi-v <t_vetter@live.de>
2026-06-28 04:27:46 +0200

    Ugly but does the job

--- python/dda/04_ergo/evaluation.py
+++ python/dda/04_ergo/evaluation.py
@@ -1,12 +1,15 @@
 from matplotlib import pyplot as plt
-from numpy import append, argmax, array, sqrt
+from numpy import append, argmax, array, mean, pi, sqrt, std, where
 from os.path import dirname, join
 from pandas import read_csv
 from tools.signals.fourier import fft
-from tools.statistics.linear_regression import Linreg
+from tools.statistics.linear_regression import Linreg, PlotLinregWithErrorAndScatterErrorbars
 from tools.statistics.uncertainty_calculation import GetResultAndUncertainty
 
 g = 9.81
+
+threshold = 1.5
+subsignal_length = 8
 
 def load_data(file):
     script_dir = dirname(__file__)
@@ -37,15 +40,19 @@ def calibration():
 def ω_to_force(fun):
     filenames = [f'{name}_{ii}' for name in ('Petros', 'Tobi') for ii in range(1, 5)]
 
-    fig, axs = plt.subplots(4, 2)
-    for file, ax in zip(filenames, axs.flatten()):
+    freq_fig, freq_axs = plt.subplots(4, 2)
+    f_means = []
+    Δfs = []
+    F_means = []
+    ΔFs = []
+    for file, ax in zip(filenames, freq_axs.flatten()):
         data = load_data(f'{file}.csv')
         t = data['Time (s)'].to_numpy()
         U = data['Ch.1 (V)']
         ω = data['Ch.2 (V)'].to_numpy()
 
         freqs, ω_ft = fft(t, ω)
-        freqs = freqs/8 # 8 magnets on the wheel
+        freqs = freqs/subsignal_length # divide by number of magnets on the wheel
         mask = freqs < 5; freqs = freqs[mask]; ω_ft = ω_ft[mask]
         max_f = freqs[argmax(ω_ft)]
         ax.plot(freqs, ω_ft, color='k')
@@ -55,14 +62,35 @@ def ω_to_force(fun):
         ax.set_ylabel('amplitude')
         ax.legend()
 
+        rising_edges = where((ω[:-1]<threshold) & (ω[1:] >= threshold))[0]
+
+        subsignals = []
+        subsignals_t = []
+        freqs2 = []
+        for ii in range(0, len(rising_edges) - subsignal_length + 1):
+            start_idx = rising_edges[ii]; end_idx = rising_edges[ii + subsignal_length - 1]
+            subsignals.append(ω[start_idx:end_idx])
+            subsignals_t.append(t[start_idx:end_idx])
+            freqs2.append(1/(t[end_idx] - t[start_idx]))
+        Δf = std(freqs2)
+
+        # print(f"{file}: f = {mean(freqs2):.3f} ± {std(freqs2):.3f}")
+
         U_mean = U.mean()
         ΔU = U.std()
 
         F, ΔF = GetResultAndUncertainty(fun, [U_mean], uncertainty=True, uncertainty_params=[ΔU])
         ax.text(0.74, 0.45, f"F = {F:.1f} ± {ΔF:.1f} N", transform=ax.transAxes,
                 bbox=dict(facecolor='white', alpha=0.7, edgecolor='k'))
-        print(f"{file}: F = {F:.1f} ± {ΔF:.1f} N")
+        # print(f"{file}: F = {F:.1f} ± {ΔF:.1f} N")
 
+        f_means.append(max_f)
+        Δfs.append(Δf)
+        F_means.append(F)
+        ΔFs.append(ΔF)
+
+    Ff_fig, Ff_ax = plt.subplots()
+    PlotLinregWithErrorAndScatterErrorbars(Ff_ax, f_means, F_means, Δfs, ΔFs, "Frequenz und Kraft", "Frequenz [Hz]", "Kraft [N]")
 
     plt.subplots_adjust(hspace=0.75)
 
