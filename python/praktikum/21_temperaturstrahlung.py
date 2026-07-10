c8b71a03160e2326334fd90330ad5bb98f7659f1
Tobias Vetter <tobias.vetter@dlr.de>
2026-07-10 13:50:18 +0200

    Initial values

---
+++ python/praktikum/21_temperaturstrahlung.py
@@ -0,0 +1,16 @@
+def Ex31_leslie():
+    delT = 0.1
+    T = [86.1, 84.9, 84]
+    black = [.337, .335, .33]
+    mirror = [.1, .1, .099]
+    white = [.335, .33, .325]
+    matt = [.105, .105, .077]
+
+def Ex32_r2_dependency():
+    pass
+
+def Ex33_coolingBoltzmann():
+    pass
+
+def Ex34_pyroBoltzmann():
+    pass
