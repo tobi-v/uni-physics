a2c757624767945a4b3263f1a8415bc7ec599b56
tobi-v <57125403+tobi-v@users.noreply.github.com>
2026-07-24 16:41:47 +0200

    Prakt/dda/01 rlc (#54)
    * RLC response function
    
    * Theoretical curve for rectangle signal

--- python/tools/electricity/RLC_circuit.py
+++ python/tools/electricity/RLC_circuit.py
@@ -1,4 +1,4 @@
-from numpy import atan2, pi, sqrt
+from numpy import atan2, cos, exp, pi, sqrt
 from numpy.typing import NDArray
 from tools.statistics.uncertainty_calculation import GetResultAndUncertainty
 
@@ -14,6 +14,14 @@ def SeriesPhase(ω: NDArray, R: float, L: float, C: float, deg: bool=False) -> N
         return result * 180/pi
     else:
         return result
+    
+def SeriesAmplitudeDecayAtR(t, u_0, R, L, C):
+    α = R/(2*L)
+    ω_0 = sqrt(1/(L*C))
+    ζ = α/ω_0
+    ω_d = ω_0 * sqrt(1 - ζ**2)
+
+    return u_0 * exp(-α*t) * cos(ω_d*t)
     
 def SeriesAmplitudeAtR(ω: NDArray, R: float, L: float, C: float, uncertainty=False, Δω=0, ΔR=0, ΔL=0, ΔC=0):
     def SeriesAmplitudeAtRInner(ω: NDArray, R: float, L: float, C: float):
