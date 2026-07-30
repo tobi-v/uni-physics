594f4db718a41e5334eb9a6d486bf5591ddda112
tobi-v <t_vetter@live.de>
2026-07-30 20:21:16 +0200

    Adjust plots a lil

--- python/dda/04_ergo/evaluation.py
+++ python/dda/04_ergo/evaluation.py
@@ -89,6 +89,7 @@ def ω_to_force(fun):
         F_means.append(F)
         ΔFs.append(ΔF)
 
+    plt.tight_layout()
     Ff_fig, Ff_ax = plt.subplots()
     coeff, cov = PlotLinregWithErrorAndScatterErrorbars(Ff_ax, f_means, F_means, Δfs, ΔFs, "Frequenz und Kraft", "Frequenz [Hz]", "Kraft [N]")
     print(f"Übertragungsfunktion: F(ω) = ({coeff[0]:.1f} ± {cov[0][0]:.1f}) ω + ({coeff[1]:.1f} ± {cov[1][1]:.1f})")
@@ -99,4 +100,4 @@ fun, coeff, cov = calibration()
 ω_to_force(fun)
 
 plt.tight_layout()
-# plt.show()
+plt.show()
