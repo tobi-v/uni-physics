9e1753da98237cbfbd7d42af6af4263eddc25dfb
tobi-v <t_vetter@live.de>
2026-08-02 09:58:06 +0200

    Fix multi product function with uncertainties

--- python/tools/maths/functions.py
+++ python/tools/maths/functions.py
@@ -1,4 +1,5 @@
-from numpy import exp, floating
+from functools import reduce
+from numpy import array, exp, floating, multiply, ndarray
 from numpy.typing import NDArray
 from tools.statistics.uncertainty_calculation import GetResultAndUncertainty
 
@@ -8,12 +9,47 @@ def Gaussian(x: NDArray[floating], amplitude: float, mean: float, stddev: float)
         raise ZeroDivisionError("Standard deviation must be non-zero.")
     return amplitude * exp(-0.5 * ((x - mean) / stddev) ** 2)
 
-def Ratio(denominator: NDArray[floating], numerator: NDArray[floating]) -> NDArray[floating]:
+def Ratio(denominator: NDArray[floating], numerator: NDArray[floating], uncertainty=False, Δdenominator=0, Δnumerator=0) -> NDArray[floating]:
     '''Berechnet das Verhältnis zweier Variablen var2/var1.'''
-    return numerator/denominator
+    def RatioInner(denominator, numerator):
+        return numerator/(denominator + 1e-10)
+    
+    return GetResultAndUncertainty(RatioInner, [denominator, numerator], uncertainty, [Δdenominator, Δnumerator])
 
 def inverse(x, uncertainty=False, Δx=0):
     def inverseInner(x):
         return 1 / (x + 1e-10)
     
     return GetResultAndUncertainty(inverseInner, [x], uncertainty, [Δx])
+
+def Sum(arr, uncertainty=False, Δarr=0):
+    def SumInner(*arr):
+        return sum(arr)
+
+    return GetResultAndUncertainty(SumInner, [arr], uncertainty, [Δarr])
+
+def Product(arr, uncertainty=False, Δarr=0):
+    def ProductInner(*arr):
+        prod = 1
+        for val in arr:
+            if isinstance(val, (list, tuple)):
+                val = array(val)
+            prod *= val
+
+        if isinstance(prod, ndarray):
+            return prod.tolist()
+        return prod
+
+    return GetResultAndUncertainty(ProductInner, [*arr], uncertainty, [*Δarr])
+
+def ElementwiseProduct(*arr, uncertainty=False, Δarr=0):
+    def ElementwiseProductInner(*arr):
+        return reduce(multiply, arr)
+
+    return GetResultAndUncertainty(ElementwiseProductInner, [*arr], uncertainty, [*Δarr] if isinstance(Δarr, (list, tuple)) else [Δarr] * len(arr))
+
+def Mean(arr, uncertainty=False, Δarr=0):
+    def MeanInner(*arr):
+        return sum(arr)/len(arr)   # Need to be done like that for correct application of uncertainty calculation
+
+    return GetResultAndUncertainty(MeanInner, [*arr], uncertainty, [*Δarr])
